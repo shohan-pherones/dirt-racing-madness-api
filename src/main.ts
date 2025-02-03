2e2941bf2d449fb8c638470650d7af90702b4a33
shohan-pherones <shohan.sub.56@gmail.com>
2025-02-03 23:18:03 +0600

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  app.enableCors({
-    origin: [
-      'https://dirt-racing-madness-client.vercel.app',
-      'http://localhost:3000',
-    ],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: [
+        'https://dirt-racing-madness-client.vercel.app',
+        'http://localhost:3000',
+      ],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+      credentials: true,
+    },
   });
-
   await app.listen(process.env.PORT ?? 4000);
 }
 void bootstrap();
